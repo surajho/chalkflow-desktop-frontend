947966bc9eee07c8ee59b530f4691295a22287f5
Suraj Ho <suraj@ho-sting.com>
2026-01-10 20:42:35 +0100

    yarn build and yarn dev now run succesfully

--- src/__tests__/component/App.test.tsx
+++ src/__tests__/component/App.test.tsx
@@ -1,5 +1,6 @@
 import { describe, it, expect, vi } from 'vitest';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import React from 'react';
 import App from '../../renderer/App';
 
 describe('Component Test Example: App Login Form', () => {
